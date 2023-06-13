f209a98696d480e11f9e82cef44d816efa98672f
V0pe <dampblew01@gmail.com>
2023-06-13 17:19:22 -0600

    Authentication testing with redux tools

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,5 @@
 import { combineReducers } from 'redux';
 
-const rootReducer = combineReducers({
+export default combineReducers({
   auth: (state, action) => 'testing',
 });
-
-export default rootReducer;
