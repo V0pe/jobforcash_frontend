690ef03a25785f0044027d6fca36747e91d06074
V0pe <dampblew01@gmail.com>
2023-06-15 10:08:29 -0600

    Implement Redux actions for Laborers

---
+++ src/redux/actions/laborers.js
@@ -0,0 +1,19 @@
+import { FETCH_LABORERS } from '.';
+import { getToken } from './auth';
+
+export const displayLaborers = () => async (dispatch) => {
+  const response = await fetch('http://localhost:3001/v1/laborers', {
+    method: 'GET',
+    headers: { 
+      'Content-Type': 'application/json',
+      Authorization: getToken(),
+     },
+  });
+  if (response.ok) {
+    const data = await response.json();
+    const laborers = data.map((laborer) => laborer.attributes);
+    dispatch({ type: FETCH_LABORERS, payload: laborers });
+    } else {
+  dispatch({ type: FETCH_LABORERS, payload: [] });
+  }
+};
