cceb19e6e5455783531e4d50edc2ce28f8a3fe6c
German Renato Cobian <germancobian@hotmail.com>
2023-06-16 12:48:34 -0700

    Create Sidebar component and style

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ProtectedRoutes from './components/ProtectedRoutes';
 import useAuth from './hooks/useAuth';
 import Signup from './components/Signup';
 import Login from './components/Login';
-import LogoutProvisional from './components/LogoutProvisional';
+import TestHomePage from './components/TestHomePage';
 
 function App() {
   const { authChecked, loggedIn } = useAuth();
@@ -16,7 +16,7 @@ function App() {
         <Route path="/login" element={<Login loggedIn={loggedIn} />} />
         <Route path="/signup" element={<Signup />} />
         <Route element={<ProtectedRoutes isAllowed={loggedIn} authChecked={authChecked} redirectPath="/login" />}>
-          <Route path="/" element={<LogoutProvisional />} />
+          <Route path="/" element={<TestHomePage />} />
         </Route>
       </Routes>
     </Router>
---
+++ src/components/Sidebar.js
@@ -0,0 +1,73 @@
+import React, { useId, useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { logoutUser } from '../redux/actions/auth';
+import Logo from '../assets/jfc-logo.png';
+import MenuHamburger from '../assets/menu.svg';
+import CloseMenu from '../assets/close.svg';
+import './components.css';
+
+function Sidebar() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const navLinks = [
+    {
+      id: useId(),
+      path: '/',
+      name: 'Test Home',
+    },
+  ];
+
+  const openMenu = (event) => {
+    event.preventDefault();
+    setIsOpen(!isOpen);
+  };
+
+  const logout = () => {
+    dispatch(logoutUser());
+    localStorage.removeItem('token');
+    navigate('/login');
+    navigate(0);
+  };
+
+  return (
+    <>
+      <div className="divH-C">
+        <button className="buttonH-C" onClick={(e) => openMenu(e)} type="button" id="menu-options">
+          <img className="iconH-C" src={isOpen ? CloseMenu : MenuHamburger} alt="hamburger-menu" />
+        </button>
+      </div>
+      <aside className={isOpen ? 'open sidebar' : 'sidebar'}>
+        <nav className="nav d-flex flex-column my-5">
+          <div className="">
+            <img className="logo mx-5" src={Logo} alt="jobforcash-logo" />
+          </div>
+          <ul className="my-5">
+            {navLinks.map(({ id, path, name }) => (
+              <li key={id}>
+                <NavLink to={path} onClick={() => setIsOpen(false)}>{name}</NavLink>
+              </li>
+            ))}
+            <li className="my-4">
+              <button
+                className="btn btn-outline-light custom-lime mx-3 border border-light"
+                type="button"
+                onClick={logout}
+              >
+                <strong>Log Out</strong>
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+    </>
+  );
+}
+
+export default Sidebar;
+
+Sidebar.defaultProps = {
+  currentUser: 'user',
+};
